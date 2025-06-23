61be397a8d164831c026d76b6b6a0b80f100e014
emmanuelhorstdev <emmanuel.marinhostuart@gmail.com>
2025-06-23 13:25:39 -0300

    Add files via upload

--- projeto_2.c
+++ projeto_2.c
@@ -156,7 +156,8 @@ int main()
 		printf("Escolha a opção desejada no menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t3 - Deletar nomes\n"); 
+		printf("\t4 - Sair do programa\n \n");
 		printf("Opção:"); //fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuario
@@ -176,6 +177,11 @@ int main()
     		
     		case 3:
     		deletar();
+			break;
+			
+			case 4:
+			printf("obrigado por utilizar o sistema!");
+			return 0;
 			break;
 			
 			default:
